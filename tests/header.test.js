b9835edb538d8f044f768f757854a3780893d0ab
ttuom1nen <ttuominen@outlook.com>
2021-12-26 22:21:24 +0200

    Add a log out button on screen test

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,7 +11,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  //await browser.close();
 });
 
 test("the header has the correct text", async () => {
@@ -25,5 +25,33 @@ test("clicking login starts oauth flow", async () => {
 
   const url = await page.url();
 
-  expect(url).toMatch(/accounts\.google\.com/)
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test.only("When signed in shows log out button", async () => {
+  const id = "6182c44f1a013e31840b17c6";
+
+  const Buffer = require("safe-buffer").Buffer;
+  const sessionObject = {
+    passport: {
+      user: id,
+    },
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionString);
+
+  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
+  expect(text).toEqual("Logout");
 });
