238433083670c49737ae89252688bae1db6feb66
ttuom1nen <ttuominen@outlook.com>
2022-01-15 22:32:50 +0200

    Add get and post methods to page

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -35,6 +35,35 @@ class CustomPage {
   async getContentsOf(selector) {
     return this.page.$eval(selector, (el) => el.innerHTML);
   }
+
+  get(path) {
+    return this.page.evaluate((_path) => {
+      return fetch(_path, {
+        method: "GET",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+    }, path);
+  }
+
+  post(path, data) {
+    this.page.evaluate(
+      (_path, _data) => {
+        return fetch(_path, {
+          method: "POST",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(_data),
+        }).then((res) => res.json());
+      },
+      path,
+      data
+    );
+  }
 }
 
 module.exports = CustomPage;
