081026e89919905363c20737b152ce0f357feb85
ttuom1nen <ttuominen@outlook.com>
2022-01-17 22:32:45 +0200

    Create ci env config

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "732144638679-90a5vopfh5fk8rk9uim9t6jegn3llt4r.apps.googleusercontent.com",
   googleClientSecret: "Lsie0wg0R7OQEYAIEK3sm-pq",
-  mongoURI: "mongodb://127.0.0.1:27017/blog_dev_local",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
   cookieKey: "264767120228",
   redisUrl: "redis://127.0.0.1:6379",
 };
