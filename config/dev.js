14715c8c33905c6738b6ad29154c904e2f65651b
ttuom1nen <ttuominen@outlook.com>
2022-02-12 16:39:35 +0200

    Remove git cache

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "732144638679-90a5vopfh5fk8rk9uim9t6jegn3llt4r.apps.googleusercontent.com",
-  googleClientSecret: "Lsie0wg0R7OQEYAIEK3sm-pq",
-  mongoURI: "mongodb://127.0.0.1:27017/blog_dev_local",
-  cookieKey: "264767120228",
-  redisUrl: "redis://127.0.0.1:6379",
-};
